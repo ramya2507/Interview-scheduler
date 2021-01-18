45e17d185b73e78d8f5e8ffa680bcd7c64a43ba5
MegaMind <ramyaa1431@gmail.com>
2021-01-18 18:50:45 -0500

    Adding button features

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,22 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
+
 export default function Button(props) {
-   return <></>;
+   const { confirm, danger, disabled, onClick, children} = props;
+
+   let buttonClass = classNames("button", {
+      " button--confirm":confirm,
+      " button--danger":danger
+   });
+
+ return <button  
+          disabled={disabled} 
+          onClick={onClick} 
+          className={buttonClass}
+         >
+            {children}
+         </button>;
 }

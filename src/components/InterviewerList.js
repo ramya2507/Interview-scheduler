4783878d935fbb2f16735b3c152ab5e2b3d47509
MegaMind <ramyaa1431@gmail.com>
2021-01-19 08:27:45 -0500

    add interview list html, css, stories

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
+
+export default function InterviewerList(props) {
+  const { interviewers } = props;
+  const interviewerData = interviewers.map(interviewer => {
+    return <InterviewerListItem
+    key = {interviewer.id}
+    id = {interviewer.id}
+    name = {interviewer.name}
+    avatar = {interviewer.avatar}
+    selected = {interviewer.id === props.interviewer}
+    setInterview = {props.setInterview}
+    />
+  })
+
+  return <section className="interviewers">
+   <h4 className="interviewers__header text--light">Interviewer</h4>
+   <ul className="interviewers__list">{interviewerData}</ul>
+  </section>;
+}

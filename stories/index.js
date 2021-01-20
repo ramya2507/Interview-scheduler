4b15b101a54494706e73855cb16607afc110da20
MegaMind <ramyaa1431@gmail.com>
2021-01-19 20:53:12 -0500

    stories for Appointment

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,9 @@ import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 //stories for Button
 storiesOf("Button", module)
   .addParameters({
@@ -144,16 +147,31 @@ storiesOf("InterviewerList", module)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
-  .add("Show", () => <Show interviewer={interviewer.name}/>)
-  .add("Show with Edit", () => (
+  .add("Show", () => (
       <Show  
         interviewer={interviewer.name} 
-        onEdit={action("onEdit")} 
+        onEdit={action("onEdit")}
+        onDelete={action("onDelete")}
         />
   ))
-  .add("Show with delete",() => (
-      <Show 
-        interviewer={interviewer.name} 
-        onDelete={action("onDelete")} 
-      />
+  .add("Confirm", () => (
+    <Confirm 
+      message={"Delete the appointment?"} 
+      onCancel={action("OnCancel")}
+      onConfirm={action("OnConfirm")}
+    /> 
+  ))
+  .add("Saving", () => <Status message={"Saving"} />)
+  .add("Deleting", () => <Status message={"Deleting"} />)
+  .add("Error Deleting", () => (
+    <Error 
+      message={"Could not delete appointment"} 
+      onClose={action("onClose")}
+    />
+  ))
+  .add("Error Saving", () => (
+    <Error 
+      message={"Could not save appointment"} 
+      onClose={action("onClose")}
+    />
   ))

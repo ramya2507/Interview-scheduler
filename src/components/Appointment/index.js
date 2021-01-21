67ed69db8feeb244940a027b9be44f2a45c40d56
MegaMind <ramyaa1431@gmail.com>
2021-01-20 22:37:32 -0500

    refactor code

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
 
 export default function Appointment(props){
-
-return <article className="appointment">
-  
-</article>;
+  const { time,interview } = props;
+ 
+  return <article className="appointment">
+        <Header time={time} />
+        {interview ? 
+          <Show 
+            student={interview.student}
+            interviewer={interview.interviewer.name}
+          /> : <Empty /> }
+  </article>;
 }

fdd34595e3245f2c775159449659551d1de174c7
MegaMind <ramyaa1431@gmail.com>
2021-01-21 13:58:59 -0500

    use promise all for use effect

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,39 +4,30 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
+import getAppointmentsForDay from "helpers/selectors";
 
 
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  }
-];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: { }
+  });
 
-  useEffect(()=> {
-    axios.get("/api/days")
-    .then((response) => {
-      setDays([...response.data]);
-      console.log(response.data);
-    });
-  },[days])
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const setDay = day => setState({ ...state, day });
+  
+  useEffect(() => {
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments")
+    ]).then(all => {
+      console.log(all)
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data }));
+    })
+  },[]);
   
   return (
     <main className="layout">
@@ -45,13 +36,13 @@ export default function Application(props) {
         <img className="sidebar--centered" src="images/logo.png" alt="Interview Scheduler"/>
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={day => setDay(day)}/>
+          <DayList days={state.days} day={state.day} setDay={day => setDay(day)}/>
         </nav>
         <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs"/>
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-        {appointments.map((appointment) => (
+        {dailyAppointments.map((appointment) => (
           <Appointment key={appointment.id} {...appointment} />
         ))}
         <Appointment key="last" time="5pm" />

e1a4fb552a112f2886bcc53e25758524f1a7edef
MegaMind <ramyaa1431@gmail.com>
2021-01-22 17:10:29 -0500

    add delete function

--- src/components/Application.js
+++ src/components/Application.js
@@ -26,13 +26,24 @@ export default function Application(props) {
   const setDay = day => setState({ ...state, day });
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   const interviewers = getInterviewersForDay(state,state.day);
+  //function to create a new interview
   const bookInterview = (id, interview) => {
     console.log(id, interview);
     const appointment = {...state.appointments[id],interview: { ...interview } };
     const appointments = {...state.appointments,[id]: appointment };
     //using axios to put data in the api
     return axios.put(`http://localhost:8001/api/appointments/${id}`,{"interview":interview})
-      .then( () => setState({...state,appointments}))  //setState({...state,appointments}))
+      .then( () => setState({...state,appointments}))  
+  }
+
+  //function to delete interview
+  const cancelInterview = (id, interview) => {
+    console.log(id,interview);
+    const appointment = {...state.appointments[id],interview: { ...interview } };
+    const appointments = {...state.appointments,[id]: appointment };
+    //using axios to delete data in the api
+    return axios.delete(`http://localhost:8001/api/appointments/${id}`)
+      .then( () => setState({...state,appointments}))    
   }
 
   const schedule = dailyAppointments.map((appointment) => {
@@ -45,6 +56,7 @@ export default function Application(props) {
         interview={interview}
         interviewers={interviewers}
         bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
       />
     );
   });
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,16 +6,19 @@ import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 import Form from "components/Appointment/Form"
 import Status from "./Status";
+import Confirm from "components/Appointment/Confirm";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVE = "SAVE";
+const CONFIRM = "CONFIRM";
 
 export default function Appointment(props){
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+  //function to save a interview
   function save(name, interviewer) {
     transition(SAVE);
     const interview = {
@@ -26,6 +29,15 @@ export default function Appointment(props){
     props.bookInterview(props.id,interview)
     .then(() =>transition(SHOW));
   }
+  //function to delete a interview
+  function cancel() {
+    const interview = {
+      student:"",
+      interviewer:null
+    }
+    props.cancelInterview(props.id,interview)
+    .then(() =>transition(EMPTY));
+  }
 
   return <article className="appointment">
         <Header time={props.time} />
@@ -33,13 +45,17 @@ export default function Appointment(props){
         {mode === SHOW && (<Show 
             student={props.interview.student}
             interviewer={props.interview.interviewer.name}
+            onDelete={() => transition(CONFIRM)}
           />)}
         {mode === CREATE && (<Form 
           interviewers={props.interviewers}
-
           onSave={save}
-          onCancel={() => back(EMPTY)}
+          onCancel={cancel}
         />)}
         {mode === SAVE && <Status message="SAVING"/>}
+        {mode === CONFIRM && <Confirm 
+          message="Do you want to delete?"
+          onConfirm={cancel}
+          onCancel={back}/>}
   </article>;
 }

721655563e72cb2afa2392aa848746947a67670f
MegaMind <ramyaa1431@gmail.com>
2021-01-21 22:57:40 -0500

    implement useVisual Effect

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,18 +1,32 @@
 import React from "react";
+import { useVisualMode } from "hooks/useVisualMode";
 import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Form from "components/Appointment/Form"
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props){
   const { time,interview } = props;
+  const { mode, transition, back } = useVisualMode(
+    interview ? SHOW : EMPTY
+  );
+
  
   return <article className="appointment">
         <Header time={time} />
-        {interview ? 
-          <Show 
+        {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+        {mode === SHOW && (<Show 
             student={interview.student}
             interviewer={interview.interviewer.name}
-          /> : <Empty /> }
+          />)}
+        {mode === CREATE && (<Form 
+          interviewers={[]}
+          onCancel={() => back(EMPTY)}
+        />)}
   </article>;
 }

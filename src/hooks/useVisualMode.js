4ad74e1d620a905c000bb4efc6f7d44a718e5bec
MegaMind <ramyaa1431@gmail.com>
2021-01-22 19:15:31 -0500

    refactored code

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -6,16 +6,16 @@ export function useVisualMode(initial) {
 
   const transition = (newMode, replace = false) => {
     if(replace){
-      setHistory([...(history.slice(0,-1)), newMode]);
+      setHistory(prev => ([...(prev.slice(0,-1)), newMode]));
     } else {
-      setHistory([...history, newMode]);
+      setHistory(prev => ([...prev, newMode]));
     }
     setMode(newMode);
   }
 
   const back = () => {
     if(history[history.length - 1] !== initial){
-      setHistory(history.slice(0,-1));
+      setHistory(prev => [...prev.slice(0,-1)]);
       setMode(history[history.length - 2]);
     }
 

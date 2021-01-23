46ba4a4ec998ed02e22f8eaeb463abcdf02e7a49
MegaMind <ramyaa1431@gmail.com>
2021-01-22 23:41:31 -0500

    update spots

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -15,11 +15,12 @@ export function useApplicationData(){
     interviewers: {}
   });
   const setDay = day => setState({ ...state, day });
+    
   const bookInterview = (id, interview) => {
     console.log(id, interview);
     const appointment = {...state.appointments[id],interview: { ...interview } };
-    const appointments = {...state.appointments,[id]: appointment };
-    //using axios to put data in the api
+    const appointments = {...state.appointments,[id]: appointment }; 
+      //using axios to put data in the api
     return axios.put(`http://localhost:8001/api/appointments/${id}`,{"interview":interview})
       .then( () => setState({...state,appointments}))  
   }
@@ -45,6 +46,11 @@ export function useApplicationData(){
       }));
     })
   },[]);
+  //function to change spot
+  useEffect(() => {
+    axios.get("/api/days")
+      .then(days => setState(state => ({ ...state, days: days.data })));
+  }, [state.appointments])
 
 
 

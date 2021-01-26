88a4c98d2dca9f9f36827606849dcbb558ba556d
MegaMind <ramyaa1431@gmail.com>
2021-01-25 22:22:16 -0500

    add validate function

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,15 +5,24 @@ import InterviewerList from "components/InterviewerList";
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
   //reset function
   const reset = () => {
     setName("");
     setInterviewer(null);
   }
-
+  //cancel function to reset
   const cancel = () => {
     reset();
     props.onCancel();
+  }
+  //function to validate input
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    props.onSave(name, interviewer);
   }
 
 
@@ -32,14 +41,16 @@ export default function Form(props) {
         placeholder="Enter Student Name"
         value={name}
         onChange={(event) => setName(event.target.value)}
+        data-testid="student-name-input"
       />
     </form>
+    <section className="appointment__validation">{error}</section>
     <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
   </section>
   <section className="appointment__card-right">
     <section className="appointment__actions">
       <Button danger onClick={() => cancel()}>Cancel</Button>
-      <Button confirm onClick={() => props.onSave(name,interviewer)}>Save</Button>
+      <Button confirm onClick={() => validate()}>Save</Button>
     </section>
   </section>
 </main>

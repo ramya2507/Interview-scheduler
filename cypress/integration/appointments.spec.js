946dbda0f6dec14330236052c7f8f8c198054d91
MegaMind <ramyaa1431@gmail.com>
2021-01-27 12:46:55 -0500

    cypress edit appointment test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -21,3 +21,18 @@ describe("Appointments", () => {
    cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
  });
+
+
+ it("should edit an interview", () => {
+  cy.get("[alt=Edit]")
+    .first()
+    .click({ force: true });
+
+  cy.get("[data-testid=student-name-input]")
+    .clear()
+    .type("Lydia Miller-Jones");
+
+  cy.contains("Save").click();
+
+  cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+});

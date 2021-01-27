492d3964c9fc56e9bf413fe12de84c5bb9ab7aec
MegaMind <ramyaa1431@gmail.com>
2021-01-26 23:16:21 -0500

    test for edit

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -52,3 +52,23 @@ it("loads data, cancels an interview and increases the spots remaining for Monda
   );
   expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
 });
+it("loads data, edits an interview and keeps the spots remaining for Monday the same", async() => {
+  const { container } = render(<Application/>);
+  await waitForElement(() => getByText(container, "Archie Cohen"));
+  const appointment = getAllByTestId(
+    container,
+    "appointment"
+  ).find(appointment => queryByText(appointment, "Archie Cohen"));
+
+  fireEvent.click(queryByAltText(appointment, "Edit"));
+  fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+    target: { value: "Lydia Miller-Jones" }
+  });
+  fireEvent.click(getByText(appointment, "Save"));
+  expect(getByText(appointment, "SAVING")).toBeInTheDocument();
+  await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+  const day = getAllByTestId(container, "day").find(day =>
+    queryByText(day, "Monday")
+  );
+  expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
+});

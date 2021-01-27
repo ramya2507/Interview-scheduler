57b7aad940be8e854f0503f38d46aba2d1a3fa87
MegaMind <ramyaa1431@gmail.com>
2021-01-27 13:14:14 -0500

    cypress appointment test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -22,17 +22,30 @@ describe("Appointments", () => {
   });
  });
 
-
- it("should edit an interview", () => {
+ it("should edit an interview", async() => {
   cy.get("[alt=Edit]")
-    .first()
-    .click({ force: true });
+  .first()
+  .click({ force: true });
 
-  cy.get("[data-testid=student-name-input]")
-    .clear()
-    .type("Lydia Miller-Jones");
+  cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
+  cy.get("[alt='Tori Malcolm']").click();
 
   cy.contains("Save").click();
 
   cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+  cy.contains(".appointment__card--show", "Tori Malcolm");
 });
+
+it("should cancel an interview", async() => {
+  cy.get("[alt=Delete]")
+    .click({ force: true });
+
+  cy.contains("Confirm").click();
+
+  cy.contains("Deleting").should("exist");
+  cy.contains("Deleting").should("not.exist");
+
+  cy.contains(".appointment__card--show", "Archie Cohen")
+    .should("not.exist");
+});
+ 

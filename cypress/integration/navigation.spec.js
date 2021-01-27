f82be8f5bb3fe033f516c74fcd15b611daceae39
MegaMind <ramyaa1431@gmail.com>
2021-01-27 01:24:52 -0500

    navigation test in cypress

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -5,4 +5,8 @@ describe("Navigation", () => {
 });
 it("should navigate to Tuesday", () => {
   cy.visit("/");
+  cy
+  .contains("[data-testid=day]","Tuesday")
+  .click()
+  .should("have.class", "day-list__item--selected");
 });
